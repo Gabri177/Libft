3f928ea8b21c28a92041f75eb5a90bccc0d99177
yugao <forestcat177@gmail.com>
2024-01-12 14:19:48 +0100

    update

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: yugao <yugao@student.42madrid.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 18:45:53 by yugao             #+#    #+#             */
-/*   Updated: 2024/01/12 05:43:17 by yugao            ###   ########.fr       */
+/*   Updated: 2024/01/12 14:18:15 by yugao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	*ft_strtrim(const char *str, const char *chars_to_trim)
 	char	*result;
 
 	ft_memset (hash, 0, 256);
+	if (!str || !chars_to_trim)
+		return (NULL);
 	while (*chars_to_trim)
 	{
 		hash[(int)(*chars_to_trim % 256)] = 1;
@@ -27,9 +29,10 @@ char	*ft_strtrim(const char *str, const char *chars_to_trim)
 	while (hash[(int)(*str % 256)] == 1)
 		str ++;
 	len = ft_strlen ((char *)str);
-	while (hash[(int)(str[len - 1] % 256)] == 1)
-		len --;
-	result = (char *) malloc (sizeof (char) * len);
+	if (len)
+		while (hash[(int)(str[len - 1] % 256)] == 1)
+			len --;
+	result = (char *) malloc (sizeof (char) * (len + 1));
 	if (!result)
 		return (0);
 	ft_strlcpy (result, (char *)str, len + 1);
@@ -39,12 +42,13 @@ char	*ft_strtrim(const char *str, const char *chars_to_trim)
 /*
 int main ()
 {
-	char t1[] = "  1111777555dfd2223331111   ";
-	char t2[] = "1 7 5 d";
+	char t1[] = "      ";
+	char t2[] = " ";
 	char *fin;
 
-	fin = strtrim (t1, t2);
+	fin = ft_strtrim ("", "");
 	printf ("---%s---\n", fin);
+	free (fin);
 	return 0;
 }
 */
