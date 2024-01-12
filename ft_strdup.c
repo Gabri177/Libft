e77a68ccdd5f26968160ec02ab52a1de9681aba1
yugao <forestcat177@gmail.com>
2024-01-12 13:30:29 +0100

    update

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: yugao <yugao@student.42madrid.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 18:57:23 by yugao             #+#    #+#             */
-/*   Updated: 2024/01/12 02:20:14 by yugao            ###   ########.fr       */
+/*   Updated: 2024/01/12 12:04:24 by yugao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,11 @@ char	*ft_strdup(const char *str)
 	new = (char *) malloc (sizeof (char) * (len + 1));
 	if (!new)
 		return (0);
-	while (i++ < len)
+	while (i < len)
+	{
 		new[i] = str[i];
+		i ++;
+	}
 	new[i] = '\0';
 	return (new);
 }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,23 +6,25 @@
 /*   By: yugao <yugao@student.42madrid.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 18:58:16 by yugao             #+#    #+#             */
-/*   Updated: 2024/01/11 19:03:29 by yugao            ###   ########.fr       */
+/*   Updated: 2024/01/12 13:28:15 by yugao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strstr(const char *haystack, const char *needle)
+size_t	ft_strstr(const char *haystack, const char *needle, size_t s)
 {
 	size_t		i;
+	size_t		len;
 	const char	*n;
 
+	len = ft_strlen (needle);
 	i = 0;
 	n = needle;
-	while (*haystack)
+	while (*haystack && i < s)
 	{
 		if (!*n)
-			return (i);
+			return (i - len);
 		else if (*n == *haystack)
 			n ++;
 		else
@@ -30,7 +32,10 @@ int	ft_strstr(const char *haystack, const char *needle)
 		haystack ++;
 		i ++;
 	}
-	return (0);
+	if (s > ft_strlen (haystack))
+		return (s);
+	else
+		return (ft_strlen (haystack));
 }
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
@@ -39,9 +44,11 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	p;
 
 	len_n = ft_strlen ((char *)needle);
-	p = ft_strstr (haystack, needle);
-	if ((p + len_n - 1) <= len && p != 0)
-		return ((char *)&haystack[p - 1]);
+	p = ft_strstr (haystack, needle, len);
+	if (!needle || !*needle)
+		return ((char *)&haystack[0]);
+	if ((p + len_n - 1) <= len)
+		return ((char *)&haystack[p]);
 	else
 		return (0);
 }
@@ -50,10 +57,9 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 int main(void)
 {
 	char str1[] = "Hello whoe";
-	char str2[] = "w";
-	char *str3;
+	char str2[] = "";
 
-	str3 = ft_strnstr(str1, str2, 10);
-	printf ("\n%s\n", str3);
+	printf ("\n%s\n", ft_strnstr("lorem ipsum dolor sit amet", "", 10));
+	printf ("\n%s\n", strnstr("lorem ipsum dolor sit amet", "", 10));
 }
 */
